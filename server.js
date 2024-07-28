7b69cbd16236e08ff989847f89a78bfd2a771963
Reyn <reynbargaso@gmail.com>
2024-07-28 21:01:18 +0800

    d

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-const mineflayer = require('mineflayer')
+const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'localhost', // Minecraft server IP
+  host: 'rcb0', // Minecraft server IP
   port: 25565,       // Minecraft server port
-  username: 'bot'    // Minecraft account username
-})
+  username: 'player'    // Minecraft account username
+});
 
 bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-  bot.chat(message)
-})
+  if (username === bot.username) return;
+  bot.chat(message);
+});
 
-bot.on('error', (err) => console.log(err))
-bot.on('end', () => console.log('Bot has ended'))
-
-git init
-git add .
-git commit -m rcb0McServer"
-git push -u origin master
-git remote add origin 
-git push -u origin master
+bot.on('error', (err) => console.log(err));
+bot.on('end', () => console.log('Bot has ended'));
