cca24bee2aa0f2208ea6f40af294959483d9ba5e
Reyn <reynbargaso@gmail.com>
2024-07-30 19:54:10 +0800

    nbm

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const serverAddress = '136.158.3.108'; // Your public IP address
 const serverPort = 19132; // Default MCPE port
-const port = process.env.PORT || 19132;
+const port = process.env.PORT || 1913;
 
 const client = new net.Socket();
 client.connect(serverPort, serverAddress, () => {
