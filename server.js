4fbf104ec0b1857155ca6f3e4f24f2729e073633
Reyn <reynbargaso@gmail.com>
2024-07-30 20:09:42 +0800

    dfg

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const net = require('net');
 // Your public IP address of the Minecraft server
 const serverAddress = '136.158.3.108'; // Update with your public IP address
 const serverPort = 19132; // Default MCPE port
+const port = process.env.PORT || 4000; // Port for your local server (not used in this script)
 
 const client = new net.Socket();
 
